cbc61f953bca381aa014c14959edc5820c9e42f4
Amanda <awake@bu.edu>
2018-11-23 22:45:32 -0500

    made some changes so that users could not change the course for a review... it did not work

--- courses/urls.py
+++ courses/urls.py
@@ -3,8 +3,16 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.ListCreateCourseView.as_view(),
+    path('',
+         views.ListCreateCourseView.as_view(),
          name='course_list'),
-    path('<int:pk>/', views.RetrieveUpdateDestroyCourseView.as_view(),
+    path('<int:pk>/',
+         views.RetrieveUpdateDestroyCourseView.as_view(),
          name='course_detail'),
+    path('<int:course_pk>/reviews/',
+         views.ListCreateReviewView.as_view(),
+         name='review_list'),
+    path('<int:course_pk>/reviews/<int:pk>/',
+         views.RetrieveUpdateDestroyReviewView.as_view(),
+         name='review_detail'),
 ]
--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import generics
 
 from . import models
@@ -8,6 +9,32 @@ class ListCreateCourseView(generics.ListCreateAPIView):
     queryset = models.Course.objects.all()
     serializer_class = serializers.CourseSerializer
 
+
 class RetrieveUpdateDestroyCourseView(generics.RetrieveUpdateDestroyAPIView):
     queryset = models.Course.objects.all()
     serializer_class = serializers.CourseSerializer
+
+
+class ListCreateReviewView(generics.ListCreateAPIView):
+    queryset = models.Review.objects.all()
+    serializer_class = serializers.ReviewSerializer
+
+    def get_queryset(self):
+        return self.queryset.filter(course_id=self.kwargs.get('course_pk'))
+
+    def perform_create(self, serializer):  # prevent changing the course that is being reviewed
+        course = get_object_or_404(
+            models.Course, pk=self.kwargs.get('course_pk'))
+        serializer.save(course=course)
+
+
+class RetrieveUpdateDestroyReviewView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = models.Review.objects.all()
+    serializer_class = serializers.ReviewSerializer
+
+    def get_object(self):
+        return get_object_or_404(
+            self.get_queryset(),
+            course_id=self.kwargs.get('course_pk'),
+            pk=self.kwargs.get('pk')
+        )
