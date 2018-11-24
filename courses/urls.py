5cbb823abb838ef0b42ab0d421244fc69a1be240
Amanda <awake@bu.edu>
2018-11-23 21:49:56 -0500

    created a retrive, update and destroy generic view

--- courses/urls.py
+++ courses/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.ListCreateCourseView.as_view(), name='course_list'),
+    path('', views.ListCreateCourseView.as_view(),
+         name='course_list'),
+    path('<int:pk>/', views.RetrieveUpdateDestroyCourseView.as_view(),
+         name='course_detail'),
 ]
--- courses/views.py
+++ courses/views.py
@@ -8,3 +8,6 @@ class ListCreateCourseView(generics.ListCreateAPIView):
     queryset = models.Course.objects.all()
     serializer_class = serializers.CourseSerializer
 
+class RetrieveUpdateDestroyCourseView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = models.Course.objects.all()
+    serializer_class = serializers.CourseSerializer
