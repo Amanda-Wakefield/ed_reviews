2d4661884ace62888c70af2610e8c981b78e5e6f
Amanda <awake@bu.edu>
2018-12-07 10:08:07 -0500

    added permissions and throttling

--- README.txt
+++ README.txt
@@ -1,1 +1,5 @@
 This is the Django REST app from Team Treehouse
+
+awake token: df069a3c4515c478ed7d873c4d55e518b78a12c5
+
+testuser token: 4199caee3c10fcce40405d4ffa388042dca10560
--- courses/views.py
+++ courses/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import get_object_or_404
 
 from rest_framework import generics
 from rest_framework import mixins
+from rest_framework import permissions
 from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
@@ -48,6 +49,7 @@ class RetrieveUpdateDestroyReviewView(generics.RetrieveUpdateDestroyAPIView):
 # this is for the V2 version of the API
 
 class CourseViewSet(viewsets.ModelViewSet):
+    permission_classes = (permissions.DjangoModelPermissions,)
     queryset = models.Course.objects.all()
     serializer_class = serializers.CourseSerializer
 
